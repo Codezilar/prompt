6c44a1ba2fc1ee29bc78fc4e3999d0926b57e922
mac <mac@macs-MacBook-Air.local>
2025-05-14 23:39:54 -0700

    debugger

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,68 +1,12 @@
-"use client"
+import { Suspense } from 'react'
+import EditForm from './edit-form'
 
-import { useEffect, useState } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
-
-import Form from '@components/Form'                     
-
-const EditPrompt = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get('id')
-
-  const [submitting, setsubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: '',
-    tag: '',
-  });
-  useEffect(() =>{
-    const getPromptDetails = async () =>{
-        try {
-            const response = await fetch(`/api/prompt/${promptId}`);
-            const data = await response.json();
-            setPost({
-                prompt: data.prompt,
-                tag: data.tag
-            });
-        } catch (error) {
-            
-        }
-    }
-    if(promptId) getPromptDetails()
-
-  }, [promptId])
-
-  const updatePrompt = async (e) => {
-    e.preventDefault();
-    setsubmitting(true);
-    if(!promptId) return alert("Prompt ID nor found!")
-    try {
-      const response = await fetch(`/api/prompt/${promptId}`, {
-        method: 'PATCH',
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
-        })
-      })
-      if(response.ok){
-        router.push('/profile');
-      }
-    } catch (error) {
-      console.log(error);
-    } finally{
-      setsubmitting(false)
-    }
-    
-  }
+export default function EditPrompt() {
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <EditForm />
+    </Suspense>
   )
 }
 
-export default EditPrompt
+export const dynamic = 'force-dynamic';
