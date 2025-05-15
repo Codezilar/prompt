f5c755dd28392bb1c01ba7e5dac6cc7041d5b6ce
mac <mac@macs-MacBook-Air.local>
2025-05-14 23:35:10 -0700

    debug

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,10 +1,68 @@
-import { Suspense } from 'react';
-import EditPrompt from './EditPrompt';
+"use client"
 
-export default function UpdatePromptPage() {
+import { useEffect, useState } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+
+import Form from '@components/Form'                     
+
+const EditPrompt = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get('id')
+
+  const [submitting, setsubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  });
+  useEffect(() =>{
+    const getPromptDetails = async () =>{
+        try {
+            const response = await fetch(`/api/prompt/${promptId}`);
+            const data = await response.json();
+            setPost({
+                prompt: data.prompt,
+                tag: data.tag
+            });
+        } catch (error) {
+            
+        }
+    }
+    if(promptId) getPromptDetails()
+
+  }, [promptId])
+
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setsubmitting(true);
+    if(!promptId) return alert("Prompt ID nor found!")
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        })
+      })
+      if(response.ok){
+        router.push('/profile');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally{
+      setsubmitting(false)
+    }
+    
+  }
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <EditPrompt />
-    </Suspense>
-  );
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
+  )
 }
+
+export default EditPrompt
